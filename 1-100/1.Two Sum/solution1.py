72ceb2c61dd8c035e6fa0ae147f5e2cd6cbee7f7
gedawei1984 <gedawei1984@gmail.com>
2021-12-20 14:13:53 +0800

    Update solution1.py

--- 1-100/1.Two Sum/solution1.py
+++ 1-100/1.Two Sum/solution1.py
@@ -4,3 +4,5 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+#Runtime: 4000 ms, faster than 26.80% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.9 MB, less than 80.53% of Python3 online submissions for Two Sum.
